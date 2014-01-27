83cc9b56273d56732998453a48eabb85c1f9f106
drwatcsuci <richard.wasniowski@csuci.edu>
2014-01-27 11:46:35 -0800

    first commit

---
+++ DemoWindow.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rich
+ */
+public class DemoWindow
+{
+    public static void main(String[] args) 
+    {
+        FirstWindow w = new FirstWindow( );
+        w.setVisible(true);
+    }
+}
---
+++ EndingListener.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rich
+ */
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class EndingListener implements ActionListener
+{
+    public void actionPerformed(ActionEvent e) 
+    {
+        System.exit(0);
+    }
+}
